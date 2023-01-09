5389024ab03c81d74d30702fbbd5e40f3fbbec19
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2023-01-09 20:13:53 +0100

    I don't know why. It should now pass all tests

--- src/middleware/helpers.ts
+++ src/middleware/helpers.ts
@@ -1,8 +1,11 @@
 import {Request} from "express";
 import levelup from "levelup";
 import leveldown from "leveldown";
+import {existsSync, mkdirSync} from "fs";
 
-export const userDB = levelup(leveldown('./userStorage'))
+export const userDB = () => {
+    return levelup(leveldown('./userStorage'));
+}
 export const parametersIncluded = <T>(req: Request, ...params: T[]) => {
     return !params.find((p: T) => req.params[p.toString()] === undefined);
 }
