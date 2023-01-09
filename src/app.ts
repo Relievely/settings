4303ea837c7d43b756e4f83b77771ea0664d1786
Marcel Dominic Bandowski <marcel.bandowski@dextermedia.net>
2023-01-09 13:50:28 +0100

    Added workflows and Dockerfile

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,19 @@
 import {routes} from "./routes";
 import * as dotenv from "dotenv";
+import * as fs from "fs";
 
 const express = require("express");
 
 export const app = express();
 
-const config = dotenv.config({path: '.env'});
+if(fs.existsSync(".env")) {
+    const config = dotenv.config({path: '.env'});
 
-if (config.error) {
-    throw config.error;
+    if (config.error) {
+        throw config.error;
+    }
+} else {
+    console.warn("No environment file detected!");
 }
 
 routes(app);
